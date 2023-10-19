0c9b42e28623c66bc62bce5abd22cd515de32c37
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-10-19 15:18:32 +0300

    Added message if user doesnt have favorited cocktails

--- src/components/cocktail/CocktailListFavorites.js
+++ src/components/cocktail/CocktailListFavorites.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import CocktailCard from "./CocktailCard";
-import uuid from "react-uuid";
-
 import { fetchCocktail } from "../../workers/CocktailService";
+
+import CocktailCard from "./CocktailCard";
 import withFavorites from "../hoc/WithFavorites";
+import uuid from "react-uuid";
 
 const CocktailListFavorites = ({ favoritesList }) => {
   const [cocktailData, setCocktailData] = useState();
@@ -21,12 +21,13 @@ const CocktailListFavorites = ({ favoritesList }) => {
 
   return (
     <>
-      {console.log("data", cocktailData)}
-
-      {cocktailData &&
+      {cocktailData ? (
         cocktailData.drinks.map((drink) => {
           return <CocktailCard key={uuid()} drink={drink}></CocktailCard>;
-        })}
+        })
+      ) : (
+        <h1>You do not have any favorited drinks.</h1>
+      )}
     </>
   );
 };
