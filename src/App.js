c85a1da5429fd30806d50bfa177490772f4aac37
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-05 14:05:06 +0200

    feat: seo tags

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import RandomCocktails from "./pages/RandomCocktails";
 import Favorites from "./pages/Favorites";
 import SingleCocktail from "./pages/SingleCocktail";
 
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
-
 function App() {
-  ReactGA.initialize("G-2F34VXJKRN");
-
-  const location = useLocation();
-  useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
-
   return (
     <BrowserRouter>
       <Routes>
